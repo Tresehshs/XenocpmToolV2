4dddf57ac5d73da36b63ab9e72067d642075a872
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-05-05 23:37:04 +0800

    Update and rename XenoToolV2.py to XenoVIPTool.py

--- XenoVIPTool.py
+++ XenoVIPTool.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from xenocpm import Tresehshs
+from xenocpmvip import Tresehshs
 
 __CHANNEL_USERNAME__ = "XenoMainChannel"
 __GROUP_USERNAME__   = "XenoMainChat"
